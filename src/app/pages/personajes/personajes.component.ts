19a560c036694b3b74eb8e6507c492ffe4caf20b
ainarasanchez <ainarasanchezrodriguez1@gmail.com>
2025-03-17 20:41:15 +0100

    Traemos la información
    Ya sea con observables, ya sea con async await

--- src/app/pages/personajes/personajes.component.ts
+++ src/app/pages/personajes/personajes.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { IPersonaje } from '../../interfaces/ipersonaje.interface';
+import { PersonajesService } from '../../services/personajes.service';
+import { IResponse } from '../../interfaces/iresponse.interface';
 
 @Component({
   selector: 'app-personajes',
@@ -7,5 +10,29 @@ import { Component } from '@angular/core';
   styleUrl: './personajes.component.css'
 })
 export class PersonajesComponent {
+  arrPersonajesObservable: IPersonaje[] = [];
+  arrPersonajesPromises: IPersonaje[] = [];
+  personajesServices = inject(PersonajesService);
 
+  async ngOnInit() {
+    /* Consumición Observables - nativo angular */
+    this.personajesServices.getAllObservable().subscribe({
+      next: (data) => {
+        this.arrPersonajesObservable = data.items
+        console.log(this.arrPersonajesObservable)
+      },
+      error: (error) => {
+        console.log(error)
+      }
+    })
+
+    /* Consumición Promises - generico javascript */
+    try {
+      let response: IResponse = await this.personajesServices.getAllPromise()
+      this.arrPersonajesPromises = response.items
+      console.log('promises', this.arrPersonajesPromises)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 }
