60a16121469760bf7cccf5354f8992bbbba0accb
ainarasanchez <ainarasanchezrodriguez1@gmail.com>
2025-03-17 20:32:09 +0100

    Inyectamos httpClient

--- src/app/services/personajes.service.ts
+++ src/app/services/personajes.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { IResponse } from '../interfaces/iresponse.interface';
+import { lastValueFrom, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonajesService {
+  // para que un servicio pueda hacer una petición tiene que inyectar el HttpClient (Tipos de inyectable que hemos visto: services, router, activatedRoute, httpClient.)
+  private httpClient = inject(HttpClient)
+  private baseUrl: string = "https://dragonball-api.com/api/characters"
 
-  constructor() { }
+  /* observables en angular - peticiones asincronas nativas de angular */
+  getAllObservable(): Observable<IResponse> {
+    return this.httpClient.get<IResponse>(this.baseUrl)
+  }
+  
+  /* promises en angular - peticiones asincronas generales en javascript */
+  getAllPromise(): Promise<IResponse> {
+    return lastValueFrom(this.httpClient.get<IResponse>(this.baseUrl))
+  }
+  
 }
